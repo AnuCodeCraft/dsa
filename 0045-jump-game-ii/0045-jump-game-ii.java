49c2c8f506a30d3cafb7f03a84f9c0a73c3ca821
Anupriya Jha <anupriya10031@gmail.com>
2025-12-18 07:31:18 +0530

    Time: 1 ms (99.15%), Space: 45.5 MB (83.42%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,38 +1,22 @@
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length - 1;
-        int coverge = 0, lastJumpIndex = 0, totalJumps = 0;
+        int n = nums.length - 1; 
+        int coverage = 0, lastJumpIndex = 0, totalJumps = 0; 
 
-        if (nums.length == 1) return 0;
+        if(nums.length == 1) return 0; 
 
-        for (int i = 0; i < n; i++) {
-            coverge = Math.max(coverge, i + nums[i]);
+        for(int i = 0; i < n; i++){
+            coverage = Math.max(coverage, i + nums[i]);
 
-            if (i == lastJumpIndex) {
-                lastJumpIndex = coverge;
+            if(i == lastJumpIndex){
+                lastJumpIndex = coverage;
                 totalJumps++;
             }
-            if (lastJumpIndex >= n) {
+
+            if(lastJumpIndex >= n){
                 return totalJumps;
             }
         }
         return totalJumps;
     }
-} 
-
-// int coverge = 0; lastJumpIndex = 0; totalJumps = 0;
-// if(nums.length == 1) return 0;
-// for(int i=0; i<n; i++){
-//     coverge = Math.max(coverge, i+nums[i]);
-
-//     if(i == lastJumpIndex){
-//         lastJumpIndex = coverge;
-//         totalJumps++;
-//     };
-
-//     if(lastJumpsIndex >= n){
-//         return totalJumps;
-//     }
-// }
-// return totalJumps;
-
+}
