b8c3ff14b0d30a9906846a7be506f491746cbfd2
Anupriya Jha <anupriya10031@gmail.com>
2026-01-19 17:25:30 +0530

    Time: 12 ms (15.71%), Space: 52.1 MB (16.5%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -2,18 +2,18 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         Set seen = new HashSet<>();
 
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 char number = board[i][j];
-                if(number != '.'){
-                    if(!seen.add(number + "in rows" + i)) return false;
-                    if(!seen.add(number + "in columns" + j)) return false;
-                    if(!seen.add(number + "in block"+ j/3 + "_" + i/3)) return false;
+                if (number != '.') {
+                    if (!seen.add(number + "in rows " + i))
+                        return false;
+                    if (!seen.add(number + "in colms " + j))
+                        return false;
+                    if (!seen.add(number + "in block" + i / 3 + "_" + j / 3))
+                        return false;
                 }
             }
         }
-
         return true;
-        
-    }
-}
+    }};
