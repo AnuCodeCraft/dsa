bfb304f2d4a373c2bab521c0af26945b176ac441
Anupriya Jha <anupriya10031@gmail.com>
2026-05-19 06:18:44 +0530

    Time: 20 ms (35.46%), Space: 46.8 MB (28.38%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,39 +1,39 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
+        if(s.length() <= 1){
             return s;
         }
-
+        
+        int len = s.length();
         String LPS = "";
-        for (int i = 1; i < s.length(); i++) {
+        for(int i = 0; i<s.length(); i++){
 
-            // odd length
+            String palindrome = "";
             int low = i;
             int high = i;
-
-            while (low >= 0 &&  high < s.length() && s.charAt(low) == s.charAt(high)) {
+            
+            // odd length
+            while(low >= 0 && high < len && s.charAt(low) == s.charAt(high)){
                 low--;
                 high++;
-
             }
-            String palindrome = s.substring(low + 1, high);
-            if (LPS.length() < palindrome.length()) {
+            palindrome = s.substring(low+1, high);
+            if(palindrome.length() > LPS.length()){
                 LPS = palindrome;
             }
 
+
+            // even length
             low = i - 1;
             high = i;
-
-            while (low >= 0 &&  high < s.length() && s.charAt(low) == s.charAt(high)) {
+            while(low >= 0 && high < len && s.charAt(low) == s.charAt(high)){
                 low--;
                 high++;
             }
-
-            palindrome = s.substring(low + 1, high);
-            if (LPS.length() < palindrome.length()) {
+            palindrome = s.substring(low+1, high);
+            if(palindrome.length() > LPS.length()){
                 LPS = palindrome;
             }
-
         }
         return LPS;
     }
