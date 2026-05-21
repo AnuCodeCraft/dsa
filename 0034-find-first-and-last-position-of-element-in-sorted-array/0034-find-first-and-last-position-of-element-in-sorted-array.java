421aa9caab5c06899b63b4299ae205413229558f
Anupriya Jha <anupriya10031@gmail.com>
2026-05-21 05:46:00 +0530

    Time: 0 ms (100%), Space: 48.1 MB (49.43%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,47 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int left = findLeftBound(nums, target);
-        int right = findRightBound(nums, target);
 
-        return new int[] { left, right };
+        int frist = findFrist(nums, target);
+        int last = findLast(nums, target);
+
+        return new int[] { frist, last };
     }
 
-    private int findLeftBound(int[] nums, int target) {
-        int index = -1, low = 0, high = nums.length - 1;
+    public int findFrist(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
             if (nums[mid] == target) {
-                index = mid;
+                ans = mid;
+                high = mid - 1; // move left
+            } else if (nums[mid] > target) {
                 high = mid - 1;
-            } else if (nums[mid] < target)
+            } else {
                 low = mid + 1;
-            else
-                high = mid - 1;
+            }
         }
-        return index;
+        return ans;
     }
 
-    private int findRightBound(int[] nums, int target) {
-        int index = -1, low = 0, high = nums.length - 1;
+    public int findLast(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
             if (nums[mid] == target) {
-                index = mid;
-                low = mid + 1;
-            } else if (nums[mid] < target)
-                low = mid + 1;
-            else
+                ans = mid;
+                low = mid + 1; // move right;
+            } else if (nums[mid] > target) {
                 high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
-
-        return index;
+        return ans;
     }
 }
