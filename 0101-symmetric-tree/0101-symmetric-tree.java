c70a4908b7252730fc2d14bff3589612a7a07d4a
Anupriya Jha <anupriya10031@gmail.com>
2026-06-29 19:49:09 +0530

    Time: 0 ms (100%), Space: 43.2 MB (92.94%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,29 +1,33 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
+
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         return dfs(root.left, root.right);
     }
 
-    private boolean dfs(TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null)
-            return true;
-        if ((root1 == null || root2 == null))
-            return false;
+    private boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
 
-        return (root1.val == root2.val) && dfs(root1.left, root2.right) && dfs(root1.right, root2.left);
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
     }
 }
