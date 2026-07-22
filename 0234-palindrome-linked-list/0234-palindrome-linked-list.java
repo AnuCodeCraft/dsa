e18b713a1ec970ed1198e8397399ff40ccdca46e
Anupriya Jha <anupriya10031@gmail.com>
2026-07-22 14:58:17 +0530

    Time: 4 ms (66.53%), Space: 94.3 MB (67.56%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,43 +10,50 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+
+        // step 1 : find the middle element
         ListNode slowPointer = head;
         ListNode fastPointer = head;
 
         while (fastPointer != null && fastPointer.next != null) {
-            fastPointer = fastPointer.next.next;
             slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
         }
 
         if (fastPointer != null) {
             slowPointer = slowPointer.next;
         }
 
+        // step2 : reverse the other half of the linked List
         slowPointer = reverse(slowPointer);
         fastPointer = head;
 
+        // step 3 : compare the both halves values
         while (slowPointer != null) {
-            if (fastPointer.val != slowPointer.val) {
+            if (slowPointer.val != fastPointer.val) {
                 return false;
             }
+            ;
 
-            fastPointer = fastPointer.next;
             slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next;
         }
 
         return true;
     }
 
-    private ListNode reverse(ListNode head) {
+    // helper function to reverse the linkedList
+    private ListNode reverse(ListNode head){
         ListNode curr = head;
         ListNode prev = null;
 
-        while (curr != null) {
+        while(curr != null){
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
         }
+
         return prev;
     }
 }
