a4bb49486497c7942cdaa71b01b132d77ee52b68
Anupriya Jha <anupriya10031@gmail.com>
2026-07-24 06:08:40 +0530

    Time: 0 ms (100%), Space: 44 MB (24.51%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,48 +1,29 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rowIdx = searchRow(matrix, target);
-        if (rowIdx != -1) {
-            return binarySearch(matrix, rowIdx, target);
-        }
-        return false;
-    }
+        int row = matrix.length;
+        int col = matrix[0].length;
 
-    public int searchRow(int[][] matrix, int target) {
+        if(matrix == null || row == 0 || col == 0) return false;
+
         int low = 0;
-        int high = matrix.length - 1;
-        int idx = matrix[0].length - 1;
+        int high = row * col - 1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        while(low <= high){
+            int mid = low + (high - low)/ 2;
+            int r = mid/col;
+            int c = mid % col; 
 
-            if (matrix[mid][0] <= target && matrix[mid][idx] >= target) {
-                return mid;
-            } else if (matrix[mid][0] > target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return -1;
-    }
+            int value = matrix[r][c];
 
-    public boolean binarySearch(int[][] matrix, int row, int target) {
-        int low = 0;
-        int high = matrix[row].length -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (matrix[row][mid] == target) {
+            if(value == target){
                 return true;
-            } else if (matrix[row][mid] > target) {
+            }else if(value > target){
                 high = mid - 1;
-            } else {
+            }else {
                 low = mid + 1;
             }
         }
 
         return false;
     }
-
 }
