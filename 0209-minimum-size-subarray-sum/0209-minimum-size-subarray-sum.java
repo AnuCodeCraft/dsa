586e7669122b9c4d8830e7e94db16ebce9fd30c4
Anupriya Jha <anupriya10031@gmail.com>
2026-08-13 06:31:01 +0530

    Time: 1 ms (99.91%), Space: 68.9 MB (85.3%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,8 +1,8 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int minLen = Integer.MAX_VALUE;
-        int sum = 0;
         int left = 0;
+        int sum = 0;
 
         for (int right = 0; right < nums.length; right++) {
             sum += nums[right];
@@ -11,9 +11,8 @@ class Solution {
                 minLen = Math.min(minLen, right - left + 1);
                 sum -= nums[left];
                 left++;
-            };
-        };
-
+            }
+        }
         return minLen == Integer.MAX_VALUE ? 0 : minLen;
     }
 }
