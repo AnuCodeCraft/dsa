f6f768dd42e979bf4fe74e16ebb29bdd1364fcfc
Anupriya Jha <anupriya10031@gmail.com>
2026-09-18 16:49:50 +0530

    Time: 56 ms (50.57%), Space: 34.9 MB (35.71%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -1,32 +1,18 @@
 class Solution:
     def minimumRounds(self, tasks: list[int]) -> int:
-        tasks.sort()
-        n = len(tasks)
-        if n==1:
-            return -1
-        cnt=1
+        mp = collections.defaultdict(int)
+        for task in tasks:
+            mp[task]+=1
         ans=0
-        # print(tasks)
-        i=1
-        while i<n:
-            while i<n and tasks[i]==tasks[i-1] :
-                i+=1
-                cnt+=1
-            # print(cnt, i, tasks[i-1], ans)
-            if cnt==1:
-                # print("===",i, tasks[i-1], cnt)
+        for key, val in mp.items():
+            if val==1:
                 return -1
-            ele = tasks[i-1]
-            if cnt%3==0:
-                ans+=cnt//3
-            elif cnt%3==2:
-                ans+=(cnt//3)+1
-            elif cnt%3==1:
-                ans+=(cnt//3)-1
-                cnt-=((cnt//3)-1)*3
-                ans+=(cnt//2)
-            cnt=1
-            i+=1
-        if tasks[n-1]!=tasks[n-2]:
-            return -1
+            if val%3==0:
+                ans+=(val//3)
+            elif val%3==2:
+                ans+=(val//3)+1
+            else:
+                ans+=((val//3)-1)
+                val-=((val//3)-1)*3
+                ans+=(val//2)
         return ans
