45395b5cd431a012e494b4bee9ce72411b9fdc45
Anupriya Jha <anupriya10031@gmail.com>
2026-09-19 16:40:57 +0530

    Time: 276 ms (19.33%), Space: 20 MB (31.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(0, n):
             c = s[i]
             mp[c]+=1
-            while mp[c]>1 and j<=i:
+            while mp[c]>1:
                 mp[s[j]]-=1
                 j+=1
             ans=max(ans, i-j+1)
